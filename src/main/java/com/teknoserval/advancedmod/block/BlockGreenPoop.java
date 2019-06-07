eedeac653682b932996c7f7797c01018dc4fc43e
TeknoServal <teknoserval@gmail.com>
2019-06-07 17:19:34 -0600

    Finished off both poop blocks.

--- src/main/java/com/teknoserval/advancedmod/block/BlockGreenPoop.java
+++ src/main/java/com/teknoserval/advancedmod/block/BlockGreenPoop.java
@@ -1,40 +1,28 @@
 package com.teknoserval.advancedmod.block;
 
-import com.teknoserval.advancedmod.AdvancedMod;
 import com.teknoserval.advancedmod.creativetab.ModCreativeTab;
 import com.teknoserval.advancedmod.init.ModBlocks;
 import com.teknoserval.advancedmod.init.ModItems;
-import com.teknoserval.advancedmod.item.ModItem;
 import com.teknoserval.advancedmod.utility.IHasModel;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
-public class ModBlock extends Block implements IHasModel {
+public class BlockGreenPoop extends ModBlock implements IHasModel {
 
-	public ModBlock(Material material) {
-
+	public BlockGreenPoop(Material material) {
 		super(material);
-		String name = "base_block";
-	/*	setUnlocalizedName(name);
+		String name = "green_poop_block";
+		setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(ModCreativeTab.ADVANCED_TAB);
 
 		ModBlocks.BLOCKS.add(this);
 		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
-*/
 	}
 
-	
-
-	@Override
-	public void registerModels() {
-
-		AdvancedMod.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
-
+	public BlockGreenPoop() {
+		this(Material.ROCK);
 	}
 
 }
--- src/main/java/com/teknoserval/advancedmod/init/ModBlocks.java
+++ src/main/java/com/teknoserval/advancedmod/init/ModBlocks.java
@@ -3,17 +3,15 @@ package com.teknoserval.advancedmod.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.teknoserval.advancedmod.block.BlockBluePoop;
+import com.teknoserval.advancedmod.block.BlockGreenPoop;
 import com.teknoserval.advancedmod.block.ModBlock;
-import com.teknoserval.advancedmod.reference.Reference;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
-	
-	public static final List<ModBlock> BLOCKS = new ArrayList<ModBlock>();
-	
 
+	public static final List<ModBlock> BLOCKS = new ArrayList<ModBlock>();
+
+	public static final ModBlock GREEN_POOP_BLOCK = new BlockGreenPoop();
+	public static final ModBlock BLUE_POOP_BLOCK = new BlockBluePoop();
 
 }
